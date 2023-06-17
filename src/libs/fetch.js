7301f49aa77e03bd387c238ab8d4b88442086a93
Ümit Beniz <umitbeniz@hotmail.com>
2023-06-17 14:43:43 +0300

    tx

--- src/libs/fetch.js
+++ src/libs/fetch.js
@@ -106,8 +106,8 @@ export default class ChainFetch {
   }
 
   async getTxsBySender(sender) {
-    // return this.get(`/cosmos/tx/v1beta1/txs?events=message.sender='${sender}'&pagination.reverse=true`)
-    return this.get(`/cosmos/tx/v1beta1/txs?events=message.sender='${sender}'&pagination.reverse=true&order_by=ORDER_BY_DESC`)
+    return this.get(`/cosmos/tx/v1beta1/txs?events=message.sender='${sender}'&pagination.reverse=true`)
+    // return this.get(`/cosmos/tx/v1beta1/txs?events=message.sender='${sender}'&pagination.reverse=true&order_by=ORDER_BY_DESC`)
   }
 
   async getTxsByRecipient(recipient) {
